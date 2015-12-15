b18768844b59665f353b3fdda42400a6800b841f
austin <awoldes@gmail.com>
2015-12-15 10:04:32 +1000

    MongoDB Connection

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,42 @@
 // server.js
 
 // modules
+
+var mongoose = require('mongoose');
+
+var db = mongoose.connection;
+
 var express = require('express');  
 var app = express();
 
-
 var logger = require("morgan");
+
+
+mongoose.connect('localhost');
+mongoose.connection.on('error', function() {
+  console.log('← MongoDB Connection Error →');
+});
+
+/*Use https://github.com/sahat/ember-sass-express-starter/blob/master/app.js as a reference*/
+
+/** Setting up DB schemas **/
+
+/*
+ * Person Schema.
+ *
+ * @type {mongoose.Schema}
+ *
+
+var personSchema = new mongoose.Schema({
+  name:  String,
+  age: Number
+});
+
+/**
+ * Person mongoose model.
+ *
+var Person = mongoose.model('Person', personSchema);*/
+
 
 // set our port
 var port = process.env.PORT || 8080;
